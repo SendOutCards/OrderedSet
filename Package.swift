fb99a3103a5591f40cbca01f08e9407bcf3cb75b
Paulo Faria <paulo.faria.rl@gmail.com>
2020-08-12 17:43:49 -0300

    Update to swift-tools-version:5.2

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
-    name: "OrderedSet"
+    name: "OrderedSet",
+    products: [
+        .library(name: "OrderedSet", targets: ["OrderedSet"]),
+    ],
+    targets: [
+        .target(name: "OrderedSet"),
+        .testTarget(name: "OrderedSetTests"),
+    ]
 )
